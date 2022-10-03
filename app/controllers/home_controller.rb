07fd14bc28f73d6b01d3ca8b4b0a8e65a1e0f691
chitranshoo Prakash <chitranshoo.prakash@jarvis.consulting>
2022-10-03 22:03:34 +0530

    user authentication added

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    if authenticate_user!
+      redirect_to posts_path
+    else
+      redirect_to new_user_session_path
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # root to: 'home#index'
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+  root to: 'home#index'
 
-  root 'posts#index'
+  # root 'posts#index'
   resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
